91af2df56731a6025b9f81add328b360ecdd39a9
Sebastian Meric de Bellefon <Sebastian.MericdeBellefon@tritondigital.com>
2015-05-31 01:00:35 -0400

    make tests property tests

--- tests.hs
+++ tests.hs
@@ -2,8 +2,12 @@
 
 module Main where
 
+import Control.Monad (replicateM_)
+import Data.Int (Int64)
+
 import Test.Tasty
-import Test.Tasty.HUnit
+import qualified Test.Tasty.QuickCheck as QC
+import qualified Test.QuickCheck.Monadic as QCM
 import qualified Data.ByteString as BS
 
 import Web.Scotty (scottyApp, middleware, get, html, raise)
@@ -14,52 +18,57 @@ import qualified Network.Wai.Test as WT
 import System.Metrics
 import qualified System.Metrics.Counter as Counter
 
-import Network.Wai.Metrics --(addWaiMetrics, metrics, WaiMetrics)
+import Network.Wai.Metrics
 
-http_get :: Application -> BS.ByteString -> IO WT.SResponse
-http_get app path =
+-- Send a GET request to a WAI Application
+http_get :: BS.ByteString -> Application -> IO WT.SResponse
+http_get path app =
   WT.runSession (WT.srequest (WT.SRequest req "")) app
       where req = WT.setRawPathInfo WT.defaultRequest path
 
-test_metrics_null :: TestTree
-test_metrics_null = testCase "New metrics must be null" (
-  do store <- newStore
-     waiMetrics <- addWaiMetrics store
-     req <- Counter.read (requestCounter waiMetrics)
-     err <- Counter.read (serverErrorCounter waiMetrics)
-     (req, err) @?= (0,0))
+-- Return the state of Wai Metrics after running n times
+-- an action over a fresh scotty server
+test_server :: (Application -> IO a) -> Int -> IO WaiMetrics
+test_server action times = do
+  store <- newStore
+  waiMetrics <- addWaiMetrics store
+  app <- scottyApp $ do
+    middleware (metrics waiMetrics)
+    get "/" $ html "Ping"
+    get "/error" $ raise "error"
+  replicateM_ times (action app)
+  return waiMetrics
 
-test_counter_scotty :: TestTree
-test_counter_scotty = testCase "Request counter must be incremented in middleware" (
-  do store <- newStore
-     waiMetrics <- addWaiMetrics store
-     app <- scottyApp $ do
-       middleware (metrics waiMetrics)
-       get "/" $ html "Ping"
-     _ <- http_get app ""
-     _ <- http_get app ""
-     requests <- Counter.read (requestCounter waiMetrics)
-     requests @?= 2)
+-- Return the number of requests after running n times
+-- an action over a fresh scotty server
+read_request_counter :: (Application -> IO a) -> Int -> IO Int64
+read_request_counter action times = do
+  waiMetrics <- test_server action times
+  requests <- Counter.read (requestCounter waiMetrics)
+  return requests
 
+-- Return the number of server errors after running n times
+-- an action over a fresh scotty server
+read_error_counter :: (Application -> IO a) -> Int -> IO Int64
+read_error_counter action times = do
+  waiMetrics <- test_server action times
+  requests <- Counter.read (requestCounter waiMetrics)
+  return requests
 
-test_error_counter_scotty :: TestTree
-test_error_counter_scotty = testCase "Error counter must be incremented in middleware" (
-  do store <- newStore
-     waiMetrics <- addWaiMetrics store
-     app <- scottyApp $ do
-       middleware (metrics waiMetrics)
-       get "/" $ raise "error"
-     _ <- http_get app ""
-     _ <- http_get app ""
-     requests <- Counter.read (serverErrorCounter waiMetrics)
-     requests @?= 2)
+test_request_counter_scotty :: QC.NonNegative Int -> QC.Property
+test_request_counter_scotty (QC.NonNegative n) =  QCM.monadicIO test
+  where test = do c <- QCM.run $ read_request_counter (http_get "") n
+                  QCM.assert $ (fromIntegral c) == n
 
+test_error_counter_scotty :: QC.NonNegative Int -> QC.Property
+test_error_counter_scotty (QC.NonNegative n) =  QCM.monadicIO test
+  where test = do c <- QCM.run $ read_error_counter (http_get "/error") n
+                  QCM.assert $ (fromIntegral c) == n
 
 tests :: TestTree
-tests = testGroup "Unit tests" [
-    test_metrics_null
-  , test_counter_scotty
-  , test_error_counter_scotty]
+tests = testGroup "Metrics tests" [
+    QC.testProperty "Request counter must be incremented in middleware" test_request_counter_scotty
+  , QC.testProperty "Error counter must be incremented in middleware" test_error_counter_scotty]
 
 main :: IO()
 main = defaultMain tests
