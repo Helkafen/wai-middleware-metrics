60818f08eaa83e8dc0b5fa55a398213171231079
Charles O'Farrell <charleso@charleso.org>
2015-09-11 09:01:08 +1000

    Add response status class counters

--- Network/Wai/Metrics.hs
+++ Network/Wai/Metrics.hs
@@ -8,6 +8,7 @@ A <http://hackage.haskell.org/package/wai WAI> middleware to collect the followi
 
 * number of requests (counter @wai.request_count@)
 * number of server errors (counter @wai.server_error_count@)
+* number of response by status code, broken down class (count @wai.response_status_xxx@)
 * latency distribution (distribution @wai.latency_distribution@)
 
 
@@ -46,11 +47,12 @@ module Network.Wai.Metrics (
 
 import Network.Wai
 import System.Metrics
+import Control.Applicative
 import Control.Monad (when)
 import Data.Time.Clock
 import qualified System.Metrics.Counter as Counter
 import qualified System.Metrics.Distribution as Distribution
-import Network.HTTP.Types.Status (statusIsServerError)
+import Network.HTTP.Types.Status (statusCode, statusIsServerError)
 
 {-|
 The metrics to feed in WAI and register in EKG.
@@ -59,6 +61,11 @@ data WaiMetrics = WaiMetrics {
   requestCounter :: Counter.Counter
  ,serverErrorCounter :: Counter.Counter
  ,latencyDistribution :: Distribution.Distribution
+ ,statusCode100Counter :: Counter.Counter
+ ,statusCode200Counter :: Counter.Counter
+ ,statusCode300Counter :: Counter.Counter
+ ,statusCode400Counter :: Counter.Counter
+ ,statusCode500Counter :: Counter.Counter
 }
 
 {-|
@@ -66,14 +73,24 @@ Register in EKG a number of metrics related to web server activity.
 
 * @wai.request_count@
 * @wai.server_error_count@
+* @wai.response_status_1xx@
+* @wai.response_status_2xx@
+* @wai.response_status_3xx@
+* @wai.response_status_4xx@
+* @wai.response_status_5xx@
 * @wai.latency_distribution@
 -}
 registerWaiMetrics :: Store -> IO WaiMetrics
-registerWaiMetrics store = do
-  req <- createCounter "wai.request_count" store
-  err <- createCounter "wai.server_error_count" store
-  tim <- createDistribution "wai.latency_distribution" store
-  return $ WaiMetrics req err tim
+registerWaiMetrics store =
+  WaiMetrics
+    <$> createCounter "wai.request_count" store
+    <*> createCounter "wai.server_error_count" store
+    <*> createDistribution "wai.latency_distribution" store
+    <*> createCounter "wai.response_status_1xx" store
+    <*> createCounter "wai.response_status_2xx" store
+    <*> createCounter "wai.response_status_3xx" store
+    <*> createCounter "wai.response_status_4xx" store
+    <*> createCounter "wai.response_status_5xx" store
 
 {-|
 Create a middleware to be added to a WAI-based webserver.
@@ -86,6 +103,12 @@ metrics waiMetrics app req respond = do
     where respond' :: UTCTime -> Response -> IO ResponseReceived
           respond' start res = do
             when (statusIsServerError $ responseStatus res) (Counter.inc (serverErrorCounter waiMetrics))
+            Counter.inc $ case statusCode $ responseStatus res of
+              s | s >= 500  -> statusCode500Counter waiMetrics
+                | s >= 400  -> statusCode400Counter waiMetrics
+                | s >= 300  -> statusCode300Counter waiMetrics
+                | s >= 200  -> statusCode200Counter waiMetrics
+                | otherwise -> statusCode100Counter waiMetrics
             end <- getCurrentTime
             Distribution.add (latencyDistribution waiMetrics) (realToFrac $ diffUTCTime end start)
             respond res
